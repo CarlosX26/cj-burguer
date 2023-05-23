b399f8f33f53ab4e160d68b6b5f5f18755acad75
Carlos Junio <cjunior1@live.com>
2023-05-23 19:18:32 -0300

    feat: add list filtered

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -7,10 +7,29 @@ import { FoodsService, IFoodList } from "src/app/services/foods.service";
 })
 export class ListProductsComponent implements OnInit {
   foodList!: IFoodList[];
+  filteredFoodList: IFoodList[] = [];
+  filter!: string;
 
   constructor(private foodsService: FoodsService) {}
 
   ngOnInit(): void {
-    this.foodsService.getFoodList().subscribe((data) => (this.foodList = data));
+    this.foodsService.getFoodList().subscribe((data) => {
+      this.foodList = data;
+      this.filteredFoodList = data;
+    });
+    this.foodsService.filter$.subscribe((filter) => {
+      this.filter = filter;
+      this.addFilter(filter);
+    });
+  }
+
+  addFilter(filter: string): void {
+    this.filteredFoodList = this.filteredFoodList.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+  removeFilter(): void {
+    this.foodsService.clearFilter();
+    this.filteredFoodList = this.foodList;
   }
 }
